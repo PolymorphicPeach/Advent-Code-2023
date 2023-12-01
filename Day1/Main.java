82bc0fbd101be6b8842e95d7dc61ad631b1e220a
Matthew Peach <103006837+PolymorphicPeach@users.noreply.github.com>
2023-12-01 01:03:14 -0600

    c

--- Day1/Main.java
+++ Day1/Main.java
@@ -6,8 +6,6 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         Main driver = new Main();
